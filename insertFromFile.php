2c73f52237429fd9ae803b47cfa070605d013f84
Geetha-github-cloud <67793546+Geetha-github-cloud@users.noreply.github.com>
2024-12-29 22:13:44 +0530

    Add files via upload

---
+++ insertFromFile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Insert HTML content from Another HTML File</title>
+	  <style>
+	  </style>
+	  
+	</head>
+	<body>
+		<div cass="container-fluid" id="content">
+			<h2>Life At KEC</h2>
+			<?php
+				include "lifeatKEC.html";
+			?>
+		</div>
+		
+	</body>
+</html>
